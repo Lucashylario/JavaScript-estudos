570b70e45959c735d4290aa38eeae4ce09d17c24
Lucashylario <lucashyfrotend@hotmail.com>
2021-02-11 08:24:28 -0300

    manipulação do DOM

---
+++ src/script.js
@@ -0,0 +1,11 @@
+// Manipulação do DOM
+
+// criar cod para escrever html dentro do JS
+const body = document.querySelector('body'); // criar const e selecionar o elemento "body"
+body.innerHTML = `
+    <h1>Hello JavaScript</h1>
+`;
+
+// cod para criar CSS
+body.style.backgroundColor = '#000';
+body.style.color = '#fff';
